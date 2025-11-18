bc04e498c26c5b5cee1ce276124411416dad20aa
mrkaluen <mrkaluen@gmail.com>
2025-11-18 18:35:55 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1072,4 +1072,4 @@ def aggiorna_grafici(anno, coltura):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8050, mode="inline")
+    app.run_server(mode='external', host='0.0.0.0', port=8888, debug=True)
